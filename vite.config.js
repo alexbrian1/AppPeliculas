bd0377f933186da2b9ae9e4c52045716465c0832
Alex Rubio <alexblans@hotmail.com>
2024-10-31 20:00:50 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://alexbrian1.github.io/AppPeliculas'
+  base:'htpps://alexbrian1.github.io/AppPeliculas/'
 })
